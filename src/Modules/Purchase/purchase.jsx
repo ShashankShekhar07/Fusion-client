432d1c74dcbca96bc0d2f15c442ddf7d8f28ea98
your_username <bhanuvenkat725@gmail.com>
2024-10-11 02:13:40 +0530

    added IndentForm FilledIndents SavedIndents ArchievedIndents Inbox

--- src/Modules/Purchase/purchase.jsx
+++ src/Modules/Purchase/purchase.jsx
@@ -1,11 +1,14 @@
-import { Text } from "@mantine/core";
-import CustomBreadcrumbs from "../../components/Breadcrumbs";
+// import { Text } from "@mantine/core";
+// import CustomBreadcrumbs from "../../components/Breadcrumbs";
+// import {Routes,Route, Router } from "react-router-dom";
+import IndentForm from "./IndentForm";
+// import Inbox from "./Inbox";
 
 function PurchasePage() {
   return (
     <>
-      <CustomBreadcrumbs />
-      <Text>Purchase Page</Text>
+      {/* <CustomBreadcrumbs /> */}
+      <IndentForm />
     </>
   );
 }
