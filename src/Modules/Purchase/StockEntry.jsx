5c9756d75ae212d4e4b5a3e0ed99490f6ea2ff4b
Sunil Kumar <130148877+ankitsunil530@users.noreply.github.com>
2024-10-13 19:30:07 +0530

    Change ViewIndent (#4)
    * add: viewindent file
    
    * add: stockentry file

--- src/Modules/Purchase/StockEntry.jsx
+++ src/Modules/Purchase/StockEntry.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-// import { PiPrinter } from "react-icons/pi";
+import { PiPrinter } from "react-icons/pi";
 import {
   Button,
   TextInput,
@@ -56,7 +56,9 @@ function StockEntry() {
               </Title>
             </Grid.Col>
             <Grid.Col span={2}>
-              <Center>{/* <PiPrinter size={28} /> */}</Center>
+              <Center>
+                <PiPrinter size={28} />
+              </Center>
             </Grid.Col>
           </Grid>
 
